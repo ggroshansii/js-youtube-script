6796aaa1038f0ac3459cd9f8a78d0a5eccc0f701
ggroshansii <garthgroshansii@gmail.com>
2022-06-06 18:24:22 -0400

    create function for testing endpoint in flask backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,25 @@
+import { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+const [response, setResponse] = useState("")
+
+useEffect(() => {
+  async function testEndpoint() {
+    const response = await fetch('http://localhost:5000/', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json' 
+      }
+    })
+    const data = await response.json();
+    setResponse(data.response);
+  }
+  testEndpoint();
+})
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,6 +27,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>RESPONSE: {response}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
